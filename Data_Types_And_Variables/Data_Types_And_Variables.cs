c984c42ea3e6b126debcdd44c879c9f26483e454
Asen Penchev <asen.penchev2@gmail.com>
2018-12-23 18:52:24 +0200

    Update on previous commit

---
+++ Data_Types_And_Variables/Data_Types_And_Variables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Data_Types_Variables
+{
+    class Data_Types_And_Variables
+    {
+        static void Main()
+        {
+            //Task1
+            ushort firstNum = 52130;
+            sbyte secondNum = -115;
+            int thirdNum = 4825932;
+            byte fourthNum = 97;
+            short fifthNum = -10000;
+
+            //Task2
+            double firstDoubleNum = 34.567839023;
+            Console.WriteLine(firstDoubleNum);
+            float firstFloatNum = 12.345f;
+            Console.WriteLine(firstFloatNum);
+            double secondDoubleNum = 8923.1234857;
+            Console.WriteLine(secondDoubleNum);
+            double secondFloatNum = 3456.091;
+            Console.WriteLine(secondFloatNum);
+
+            //Task3
+            int hexadecimalInt = 0xFE;
+            Console.WriteLine(hexadecimalInt);
+
+            //Task4
+            char hexadecimalChar = (char)0x2A;
+            Console.WriteLine(hexadecimalChar);
+        }
+    }
+}
---
+++ Intro-Programming-Homework/IntroductionToProgramming.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Intro_Programming_Homework
+{
+    class IntroductionToProgramming
+    {
+        static void Main()
+        {
+            //Task 1-8
+            Console.WriteLine("Hello, C#!");
+            Console.WriteLine("Asen Penchev");
+            Console.WriteLine("1 \n101 \n1001");
+            Console.WriteLine("Asen \nPenchev");
+            Console.WriteLine(Math.Sqrt(12345));
+
+            //Task 9, 16
+            int sequenceChanger = 5;
+            int startSequence = 2;
+            for (int i = 0; i < 10; i++, sequenceChanger += 2)
+            {
+                Console.WriteLine(startSequence);
+                if (i % 2 == 0)
+                {
+                    startSequence -= sequenceChanger;
+                }
+                else
+                {
+                    startSequence += sequenceChanger;
+                }
+            }
+
+            //Task 14
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Year);
+
+            //Task 15
+            string date = Console.ReadLine();
+            String[] components = date.Split('.');
+            int month = int.Parse(components[0]);
+            int day = int.Parse(components[1]);
+            int year = int.Parse(components[2]);
+            if (month <= DateTime.Now.Month)
+            {
+                if (day > DateTime.Now.Day)
+                {
+                    year++;
+                }
+            }
+            int userAge = DateTime.Now.Year - year;
+            Console.WriteLine("You are " + userAge + " years old");
+            int userAgeInTenYears = userAge + 10;
+            Console.WriteLine("In 10 years you will be " + userAgeInTenYears + " years old");
+        }
+    }
+}
