4deade0e4563ada6c7844aab2813d97b84e9bb62
AYPenchev <asen.penchev2@gmail.com>
2019-04-12 12:25:50 +0300

    CSharp Advanced - Homework 2

---
+++ CSharp_Advanced/Methods/Methods.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Linq;
+
+namespace Methods
+{
+    class Methods
+    {
+
+        public static void SayHello01(string userName)
+        {
+            Console.WriteLine("Hello, " + userName + "!");
+        }
+
+        public static string AsksForName01()
+        {
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        public static int GetMax02(int firstNumber, int secondNumber)
+        {
+            return firstNumber >= secondNumber ? firstNumber : secondNumber;
+        }
+
+        public static string EnglishDigit03(int lastDigit)
+        {
+            switch (GetLastDigit03(lastDigit))
+            {
+                case 0: return "Zero";
+                case 1: return "One";
+                case 2: return "Two";
+                case 3: return "Three";
+                case 4: return "Four";
+                case 5: return "Five";
+                case 6: return "Six";
+                case 7: return "Seven";
+                case 8: return "Eight";
+                case 9: return "Nine";
+
+                default: return "Something went wrong! Try again!";
+            }
+
+        }
+        public static int GetLastDigit03(int number)
+        {
+            return number % 10;
+        }
+
+        public static int AppearanceCount04(int[] numbers, int numberCheckedForAppearance)
+        {
+            int countAppearance = 0;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                if (numberCheckedForAppearance == numbers[i])
+                {
+                    countAppearance++;
+                }
+            }
+            return countAppearance;
+        }
+
+        public static void FillArray(ref int[] numbers)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+
+            numbers = Console.ReadLine()
+                           .Split(new char[] { ' ' }, arraySize, StringSplitOptions.RemoveEmptyEntries)
+                           .Select(item => int.Parse(item))
+                           .ToArray();
+        }
+
+        public static int LargerThanNeighbours05(int[] largerThanNeighbourArray)
+        {
+            int countLargerThanNeighbours = 0;
+
+            for (int i = 0; i < largerThanNeighbourArray.GetLongLength(0); i++)
+            {
+                if (i != 0 && i != largerThanNeighbourArray.GetLongLength(0) - 1)
+                {
+                    if (largerThanNeighbourArray[i] > largerThanNeighbourArray[i - 1] && largerThanNeighbourArray[i] > largerThanNeighbourArray[i + 1])
+                    {
+                        countLargerThanNeighbours++;
+                    }
+                }
+                /* Check if first and last numbers are larger than their only neighbour
+                else if (i == 0)
+                {
+                    if (largerThanNeighbourArray[i] > largerThanNeighbourArray[i + 1])
+                    {
+                        countLargerThanNeighbours++;
+                    }
+                }
+                else if (i == largerThanNeighbourArray.GetLongLength(0) - 1)
+                {
+                    if (largerThanNeighbourArray[i] > largerThanNeighbourArray[i - 1])
+                    {
+                        countLargerThanNeighbours++;
+                    }
+                }
+                */
+            }
+            return countLargerThanNeighbours;
+        }
+
+        static void Main()
+        {
+            /* Task 1
+             string userName = AsksForName01();
+             SayHello01(userName);
+            //SayHello01(AsksForName01());
+            */
+
+            /* Task 2
+            int[] numbers = Console.ReadLine()
+                           .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                           .Select(item => int.Parse(item))
+                           .ToArray();
+
+            int firstNumber = numbers[0];
+            int secondNumber = numbers[1];
+            int thirdNumber = numbers[2];
+
+            Console.WriteLine(GetMax02(GetMax02(firstNumber, secondNumber), thirdNumber));
+            */
+
+            /* Task 3 
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(EnglishDigit03(GetLastDigit03(number)));
+            */
+
+            /* Task 4
+            int[] numbers = { };
+            
+            FillArray(ref numbers);
+
+            int numberCheckedForAppearance = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(AppearanceCount04(numbers, numberCheckedForAppearance));
+            */
+
+            /* Task 5
+            int[] largerThanNeighbourArray = { };
+
+            FillArray(ref largerThanNeighbourArray);
+
+            Console.WriteLine(LargerThanNeighbours05(largerThanNeighbourArray));
+            */
+
+            /* Task 6 */
+        }
+    }
+}
