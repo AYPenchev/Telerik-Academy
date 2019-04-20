91136912304cfe0db805526c0cd151471169f39b
AYPenchev <asen.penchev2@gmail.com>
2019-04-20 17:05:37 +0300

    CSharp Advanced - Homework 3 - Task 2

--- CSharp_Advanced/Using_Classes_and_Objects/UsingClassesAndObjects.cs
+++ CSharp_Advanced/Using_Classes_and_Objects/UsingClassesAndObjects.cs
@@ -16,11 +16,23 @@ namespace UsingClassesAndObjects
             }
         }
 
+        public static void PrintTenRandomNumbers02()
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", randNum.Next(100, 200));
+            }
+        }
+
         static void Main()
         {
             /* Task 1
             int year = int.Parse(Console.ReadLine());
             IsLeapYear01(year);
+            */
+            /* Task 2 
+            PrintTenRandomNumbers02();
             */
         }
     }
