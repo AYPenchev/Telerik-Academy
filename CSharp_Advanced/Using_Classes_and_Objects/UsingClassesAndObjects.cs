ef0ae3590d4cbd2331fad661fdcd573be30066e3
AYPenchev <asen.penchev2@gmail.com>
2019-04-20 16:54:13 +0300

    CSharp Advanced - Homework 3 - Task 1

--- CSharp_Advanced/Using_Classes_and_Objects/UsingClassesAndObjects.cs
+++ CSharp_Advanced/Using_Classes_and_Objects/UsingClassesAndObjects.cs
@@ -4,8 +4,24 @@ namespace UsingClassesAndObjects
 {
     class UsingClassesAndObjects
     {
+        public static void IsLeapYear01(int year)
+        {
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("Leap");
+            }
+            else
+            {
+                Console.WriteLine("Common");
+            }
+        }
+
         static void Main()
         {
+            /* Task 1
+            int year = int.Parse(Console.ReadLine());
+            IsLeapYear01(year);
+            */
         }
     }
 }
