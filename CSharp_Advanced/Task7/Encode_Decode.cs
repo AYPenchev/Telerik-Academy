c5c9db2fc6f4d1bd44441e47d81a190bb04edc72
Asen Penchev <asen.penchev2@gmail.com>
2019-04-26 12:28:04 +0300

    CSharp Advanced - Homework 4 - Task 7
    In progress

---
+++ CSharp_Advanced/Task7/Encode_Decode.cs
@@ -0,0 +1,57 @@
+﻿namespace Task7
+{
+    using System;
+
+    class EncodeDecode
+    {
+        public static string EncodeString(string cryptKey, string stringToBeEncoded)
+        {
+            string encodedString = string.Empty;
+            for (int i = 0, j = 0; i < stringToBeEncoded.Length; i++, j++)
+            {
+                if (cryptKey.Length - 1 == j)
+                {
+                    j = 0;
+                }
+
+                encodedString += stringToBeEncoded[i] ^ cryptKey[j];
+            }
+            return encodedString;
+        }
+
+        public static string DecodeString(string cryptKey, string stringToBeDecoded)
+        {
+            string decodedString = string.Empty;
+            for (int i = 0, j = 0; i < stringToBeDecoded.Length; i++, j++)
+            {
+                if (cryptKey.Length - 1 == j)
+                {
+                    j = 0;
+                }
+
+                decodedString += stringToBeDecoded[i] ^ cryptKey[j];
+            }
+            
+            for (int i = 0, j = 0; i < stringToBeDecoded.Length; i++, j++)
+            {
+                if (cryptKey.Length - 1 == j)
+                {
+                    j = 0;
+                }
+
+                decodedString += stringToBeDecoded[i] ^ cryptKey[j];
+            }
+            return decodedString;
+        }
+
+        static void Main()
+        {
+            string cryptKey = "A@1$#f";
+
+            string stringToBeEncoded = Console.ReadLine();
+            string encodedString = EncodeString(cryptKey, stringToBeEncoded);
+            Console.WriteLine(encodedString);
+            //string
+        }
+    }
+}
