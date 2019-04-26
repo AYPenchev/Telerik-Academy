1c8a29c92bc3c0c41d182e6a07c78905a53b5efa
Asen Penchev <asen.penchev2@gmail.com>
2019-04-26 09:31:47 +0300

    CSharp Advanced - Homework 4 - Task 3

--- CSharp_Advanced/Task3/Correct_Brackets.cs
+++ CSharp_Advanced/Task3/Correct_Brackets.cs
@@ -1,11 +1,71 @@
 ﻿namespace Task3
 {
     using System;
+    using System.Collections.Generic;
 
     class CorrectBrackets
     {
+        public static bool AreBracketsCorrect(string expression)
+        {
+            Queue<char> allBrackets = new Queue<char>();
+            int countLeftBrackets = 0;
+            int countRightBrackets = 0;
+
+            foreach (char symbol in expression)
+            {
+                if(symbol == '(')
+                {
+                    allBrackets.Enqueue(symbol);
+                    countLeftBrackets++;
+                }
+                else if(symbol == ')')
+                {
+                    allBrackets.Enqueue(symbol);
+                    countRightBrackets++;
+                }
+            }
+
+            if (countLeftBrackets != countRightBrackets)
+            {
+                return false;
+            }
+
+            char currentBracket;
+
+            for (int i = 0; i < allBrackets.Count; i++)
+            {
+                if (countRightBrackets < countLeftBrackets)
+                {
+                    return false;
+                }
+
+                currentBracket = allBrackets.Dequeue();
+
+                if (currentBracket == '(')
+                {
+                    countLeftBrackets--;
+                }
+                else
+                {
+                    countRightBrackets--;
+                }
+            }
+            return true;
+        }
+
         static void Main()
         {
+            string expression = Console.ReadLine();
+            bool areBracketsCorrect = AreBracketsCorrect(expression);
+
+            if(areBracketsCorrect)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
         }
     }
 }
