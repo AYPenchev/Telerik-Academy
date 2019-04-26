2a10bd2f1330e703c89f11c5828c18a8538fada2
Asen Penchev <asen.penchev2@gmail.com>
2019-04-26 18:26:03 +0300

    CSharp Advanced - Homework 4 - Task 8

---
+++ CSharp_Advanced/Task8/Extract_Sentences.cs
@@ -0,0 +1,29 @@
+﻿namespace Task8
+{
+    using System;
+    using System.Text;
+
+    class ExtractSentences
+    {
+        public static string SentenceExtract(string text, string searchedWordInSenteces)
+        {
+            string[] textAsArray = text.Split('.');
+            StringBuilder searchedSentences = new StringBuilder();
+            foreach (string sentence in textAsArray)
+            {
+                if (sentence.Contains(" " + searchedWordInSenteces + " "))
+                {
+                    searchedSentences.Append(sentence);
+                    searchedSentences.Append(".");
+                }
+            }
+            return searchedSentences.ToString();
+        }
+        static void Main()
+        {
+            string searchedWordInSenteces = Console.ReadLine();
+            string text = Console.ReadLine();
+            Console.WriteLine(SentenceExtract(text, searchedWordInSenteces));
+        }
+    }
+}
