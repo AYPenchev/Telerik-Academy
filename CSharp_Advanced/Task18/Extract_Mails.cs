614c58c08461013a57437f2818a08437eb4846d2
Asen Penchev <asen.penchev2@gmail.com>
2019-05-06 02:49:10 +0300

    CSharp Advanced - Homework 4 - Task 18

---
+++ CSharp_Advanced/Task18/Extract_Mails.cs
@@ -0,0 +1,37 @@
+﻿namespace Task18
+{
+    using System;
+    using System.Net.Mail;
+    using System.Text;
+
+    class ExtractMails
+    {
+        public static bool IsMail(string emailAddress)
+        {
+            try
+            {
+                MailAddress email = new MailAddress(emailAddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+        static void Main()
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder emails = new StringBuilder();
+
+            foreach (string email in text)
+            {
+                if(IsMail(email))
+                {
+                    emails.Append(email + "\n");
+                }
+            }
+            Console.WriteLine(emails);
+        }
+    }
+}
