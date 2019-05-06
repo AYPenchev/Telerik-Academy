bc564a29747987d04db9eedc528b9a42fe244c98
Asen Penchev <asen.penchev2@gmail.com>
2019-05-06 17:30:32 +0300

    CSharp Advanced - Homework 4 - Task 25

---
+++ CSharp_Advanced/Task24/Order_Words.cs
@@ -0,0 +1,18 @@
+﻿namespace Task24
+{
+    using System;
+
+    class OrderWords
+    {
+        static void Main()
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(text);
+
+            foreach (string word in text)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ CSharp_Advanced/Task25/Extract_Text_HTML.cs
@@ -0,0 +1,18 @@
+﻿namespace Task25
+{
+    using System;
+
+    class ExtractTextHTML
+    {
+        static void Main()
+        {
+            string inputHTML = Console.ReadLine();
+
+            string text = inputHTML.Remove(0, inputHTML.IndexOf("<tittle>") + 8);
+            text = text.Remove(text.IndexOf("</tittle>"));
+            Console.WriteLine("Tittle: " + text);
+
+            
+        }
+    }
+}
