755d6abc3fb9e8e319ec53950bd986635010e788
Asen Penchev <asen.penchev2@gmail.com>
2019-05-06 15:22:22 +0300

    CSharp Advanced - Homework 4 - Task 19

---
+++ CSharp_Advanced/Task20/Extract_Palindromes.cs
@@ -0,0 +1,42 @@
+﻿namespace Task20
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ExtractPalindromes
+    {
+        private static bool IsPalindrome(string palindrome)
+        {
+            int i = 0;
+            int j = palindrome.Length - 1;
+
+            while (i < j)
+            {
+                if (palindrome[i] != palindrome[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> extractedPalindromes = new List<string>();
+
+            foreach (string palindrome in text)
+            {
+                if(IsPalindrome(palindrome))
+                {
+                    extractedPalindromes.Add(palindrome);
+                    Console.Write(palindrome + "\n");
+                }
+            }
+
+        }
+    }
+}
