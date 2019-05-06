d7d8d2327486a8bbe80bc86eca64a2663f48585f
Asen Penchev <asen.penchev2@gmail.com>
2019-05-06 12:37:10 +0300

    CSharp Advanced - Homework 4 - Task 19

---
+++ CSharp_Advanced/Task19/Extract_Dates.cs
@@ -0,0 +1,40 @@
+﻿namespace Task19
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    class ExtractDates
+    {
+        static void Main()
+        {
+            string[] text = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            List<DateTime> dates = new List<DateTime>();
+            string format = "dd.MM.yyyy";
+
+            foreach (string searchedDate in text)
+            {
+                DateTime dateToBeAdded;
+                if (DateTime.TryParseExact(searchedDate, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateToBeAdded))
+                {
+                    dates.Add(dateToBeAdded);
+                }
+            }
+
+            foreach (DateTime date in dates)
+            {
+                Console.WriteLine(date.Year + "-" + date.Month + "-" + date.Day);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
