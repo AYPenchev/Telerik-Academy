fb21c691e7d896bf5a4b096947f2a40a47adef6c
Asen Penchev <asen.penchev2@gmail.com>
2019-05-10 02:35:08 +0300

    CSharp Advanced - Homework 5 - Task 3

---
+++ CSharp_Advanced/Exceptions/Task2/Enter_Numbers.cs
@@ -0,0 +1,49 @@
+﻿namespace Task2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class EnterNumbers
+    {
+        private static bool IsIncreasing(List<int> numbers)
+        {
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1].CompareTo(numbers[i]) >= 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main()
+        {
+            int start = 0;
+            int end = 100;
+            const int count = 10;
+
+            List<int> numbers = new List<int>();
+
+            try
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    numbers.Add(int.Parse(Console.ReadLine()));
+                }
+
+                if (numbers.Any(x => x < start) || numbers.Any(x => x > end) || !IsIncreasing(numbers))
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine("1 < " + string.Join(" < ", numbers) + " < 100");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+
+        }
+    }
+}
---
+++ CSharp_Advanced/Exceptions/Task3/Read_File_Contents.cs
@@ -0,0 +1,36 @@
+﻿namespace Task3
+{
+    using System;
+    using System.IO;
+    class ReadFileContents
+    {
+        static void Main()
+        {
+            Console.Write("Enter the full path of the file you want to read: ");
+            string filePath = Console.ReadLine();
+
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine("The content of the file is: ");
+                Console.WriteLine(fileContent);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file '{0}' was not found!", filePath);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("No file path is given!");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("The entered file path is not correct!");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("The chosen file is not supported!");
+            }
+        }
+    }
+}
