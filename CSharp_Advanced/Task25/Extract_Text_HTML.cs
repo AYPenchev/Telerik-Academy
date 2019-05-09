65817e52362029b5fed10d5c1d1e75f665bf771a
Asen Penchev <asen.penchev2@gmail.com>
2019-05-09 16:32:55 +0300

    CSharp Advanced - Homework 4 - Task 25

--- CSharp_Advanced/Task25/Extract_Text_HTML.cs
+++ CSharp_Advanced/Task25/Extract_Text_HTML.cs
@@ -1,16 +1,69 @@
 ﻿namespace Task25
 {
     using System;
+    using System.Text;
 
     class ExtractTextHTML
     {
         static void Main()
         {
-            string inputHTML = Console.ReadLine();
+            string htmlCode = @"<html>
+          <head><title>News</title></head>
+          <body><p><a href=""http://academy.telerik.com"">Telerik
+            Academy</a>aims to provide free real-world practical
+            training for young people who want to turn into
+            skilful .NET software engineers.</p></body>
+        </html>";
 
-            string text = inputHTML.Remove(0, inputHTML.IndexOf("<tittle>") + 8);
-            text = text.Remove(text.IndexOf("</tittle>"));
-            Console.WriteLine("Tittle: " + text);
+            StringBuilder extractedText = new StringBuilder();
+
+            for (int i = 0; i < htmlCode.Length; i++)
+            {
+                if (htmlCode[i] == '<')
+                {
+                    extractedText.Append(" ");
+
+                    while (htmlCode[i] != '>')
+                    {
+                        i++;
+                    }
+
+                    continue;
+                }
+
+                extractedText.Append(htmlCode[i]);
+            }
+
+            string[] output = extractedText.ToString()
+                .Split(new string[] { " ", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                //.ToArray();
+
+            Console.WriteLine("Title: {0}", output[0]);
+
+            Console.Write("Text: ");
+            for (int i = 1; i < output.Length; i++)
+            {
+                if (i != output.Length - 1)
+                {
+                    Console.Write(output[i] + " ");
+                }
+                else
+                {
+                    Console.Write(output[i]);
+                }
+            }
+
+            Console.WriteLine();
+
+
+
+
+            //string inputHTML = Console.ReadLine();
+
+            //string text = inputHTML.Remove(0, inputHTML. IndexOf("<tittle>") + 8);
+
+            //text = text.Remove(text.IndexOf("</tittle>"));
+            //Console.WriteLine("Tittle: " + text);
 
             
         }
