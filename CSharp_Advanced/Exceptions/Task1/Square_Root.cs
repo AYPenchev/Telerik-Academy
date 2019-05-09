186e7515cd37512346d64088f89f6a231b444358
Asen Penchev <asen.penchev2@gmail.com>
2019-05-09 20:23:16 +0300

    CSharp Advanced - Homework 5 - Task 1

--- CSharp_Advanced/Exceptions/Task1/Square_Root.cs
+++ CSharp_Advanced/Exceptions/Task1/Square_Root.cs
@@ -6,6 +6,20 @@
     {
         static void Main()
         {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                double sqrtNumber = Math.Sqrt(number);
+                Console.WriteLine("{0:0.000}", sqrtNumber);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
         }
     }
 }
