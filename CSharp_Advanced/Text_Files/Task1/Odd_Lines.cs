28f609bba3bb54f0f4f9bda2d9ec280d9fb96468
Asen Penchev <asen.penchev2@gmail.com>
2019-05-10 10:20:09 +0300

    CSharp Advanced - Homework 6 - Task 1

---
+++ CSharp_Advanced/Text_Files/Task1/Odd_Lines.cs
@@ -0,0 +1,47 @@
+﻿namespace Task1
+{
+    using System;
+    using System.IO;
+
+    class OddLines
+    {
+        static void Main()
+        {
+            StreamReader reader = new StreamReader("../../../Documentation.txt");
+            try
+            {
+                int rowCounter = 1;
+                while (!reader.EndOfStream)
+                {
+                    if(rowCounter % 2 != 0)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                        rowCounter++;
+                    }
+                    reader.ReadLine();
+                    rowCounter++;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("No file path is given!");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("The entered file path is not correct!");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("The chosen file is not supported!");
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+    }
+}
