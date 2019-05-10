56e7e7301f01b0834aca8ab609a0cfe68cfca03f
Asen Penchev <asen.penchev2@gmail.com>
2019-05-10 19:02:04 +0300

    CSharp Advanced - Homework 6 - Task 6

--- CSharp_Advanced/Text_Files/Task6/Save_Sorted_Names.cs
+++ CSharp_Advanced/Text_Files/Task6/Save_Sorted_Names.cs
@@ -3,16 +3,24 @@
     using System;
     using System.IO;
     using System.Linq;
+    using System.Text;
 
     class SaveSortedNames
     {
         static void Main()
         {
+            using (var writerToInputFile = new StreamWriter("input.txt"))
+            {
+                writerToInputFile.WriteLine("Ivan");
+                writerToInputFile.WriteLine("Peter");
+                writerToInputFile.WriteLine("Maria");
+                writerToInputFile.WriteLine("George");
+            }
             string[] arrayNames = File.ReadAllText("input.txt")
-                                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                .Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
                                 .Select(item => item)
                                 .ToArray();
-            
+
             Array.Sort(arrayNames);
 
             File.WriteAllLines("output.txt", arrayNames);
