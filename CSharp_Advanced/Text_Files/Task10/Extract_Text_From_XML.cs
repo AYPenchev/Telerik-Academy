48d26e039161bb92c08ba64919038da4fabe49df
Asen Penchev <asen.penchev2@gmail.com>
2019-05-11 12:27:41 +0300

    CSharp Advanced - Homework 6 - Task 10

---
+++ CSharp_Advanced/Text_Files/Task10/Extract_Text_From_XML.cs
@@ -0,0 +1,42 @@
+﻿namespace Task10
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    class ExtractTextFromXML
+    {
+        static void Main()
+        {
+            StringBuilder textContent = new StringBuilder();
+            StringBuilder xmlCode = new StringBuilder();
+
+            using (var readXML = new StreamReader("../../ExtractTextFrom.xml"))
+            {
+                while (!readXML.EndOfStream)
+                {
+                    xmlCode.Append(readXML.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < xmlCode.Length; i++)
+            {
+                if (xmlCode[i] == '<')
+                { 
+                    while (xmlCode[i] != '>')
+                    {
+                        i++;
+                    }
+                    continue;
+                }
+
+                textContent.Append(xmlCode[i]);
+            }
+
+            string extractedText = textContent.ToString();
+            string[] extractedTextAsArray = extractedText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            extractedText = string.Join(" ", extractedTextAsArray);
+            Console.WriteLine(extractedText);
+        }
+    }
+}
