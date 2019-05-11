b5efa6971c3cc1dc006b6e030118bd12a89455f6
Asen Penchev <asen.penchev2@gmail.com>
2019-05-11 11:28:32 +0300

    CSharp Advanced - Homework 6 - Task 9

---
+++ CSharp_Advanced/Text_Files/Task9/Delete_Odd_Lines.cs
@@ -0,0 +1,43 @@
+﻿namespace Task9
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    class DeleteOddLines
+    {
+        static void Main()
+        {
+            using (var firstWriteStream = new StreamWriter("../../DeleteOddLines.txt"))
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    firstWriteStream.WriteLine(i);
+                }
+            }
+
+            StringBuilder content = new StringBuilder();
+
+            using (var reader = new StreamReader("../../DeleteOddLines.txt"))
+            {
+                int rowCounter = 1;
+
+                while (!reader.EndOfStream)
+                {
+                    if (rowCounter % 2 == 0)
+                    {
+                        content.Append(reader.ReadLine() + Environment.NewLine);
+                        rowCounter++;
+                    }
+                    reader.ReadLine();
+                    rowCounter++;
+                }
+            }
+
+            using (var secondWriteStream = new StreamWriter("../../DeleteOddLines.txt"))
+            {
+                secondWriteStream.WriteLine(content);
+            }
+        }
+    }
+}
