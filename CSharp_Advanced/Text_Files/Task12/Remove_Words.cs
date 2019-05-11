0aaceacc4797dff7d90b55ac677cbf1f5c263ccc
Asen Penchev <asen.penchev2@gmail.com>
2019-05-11 15:58:04 +0300

    CSharp Advanced - Homework 6 - Task 12

--- CSharp_Advanced/Text_Files/Task12/Remove_Words.cs
+++ CSharp_Advanced/Text_Files/Task12/Remove_Words.cs
@@ -7,15 +7,39 @@
     {
         static void Main()
         {
+            string[] inputWordsToRemove = Console.ReadLine()
+                                     .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
             using (var writeWordsToRemove = new StreamWriter("../../wordsToBeRemoved.txt"))
             {
-                writeWordsToRemove.WriteLine("nice");
-                writeWordsToRemove.WriteLine("wednesday");
-                writeWordsToRemove.WriteLine("today");
+                for (int i = 0; i < inputWordsToRemove.Length; i++)
+                {
+                    writeWordsToRemove.WriteLine(inputWordsToRemove[i]);
+                }
             }
 
+            try
+            {
+                File.ReadAllText("../../wordsToBeRemoved.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("No file path is given!");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("The entered file path is not correct!");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("The chosen file is not supported!");
+            }
             string[] wordsToBeRemoved = File.ReadAllText("../../wordsToBeRemoved.txt")
-                            .Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                                            .Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
             using (var writeToFile = new StreamWriter("../../file.txt"))
             {
@@ -24,11 +48,32 @@
                                       "Pa nice aute irure dolor in today in nice velit esse cillum dolore eu fugiat nulla pariatur." + Environment.NewLine +
                                       "Excepteur sint nice today non proident, sunt in nice qui officia deserunt mollit nice id est laborum.");
             }
+
+            try
+            {
+                File.ReadAllText("../../file.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("No file path is given!");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("The entered file path is not correct!");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("The chosen file is not supported!");
+            }
             string fileText = File.ReadAllText("../../file.txt");
 
             for (int i = 0; i < wordsToBeRemoved.Length; i++)
             {
-                if(fileText.Contains(wordsToBeRemoved[i]))
+                if (fileText.Contains(wordsToBeRemoved[i]))
                 {
                     fileText = fileText.Replace(wordsToBeRemoved[i] + " ", string.Empty);
                 }
