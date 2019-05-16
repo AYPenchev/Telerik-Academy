1049655d86dc41dc996f0c34552823a7fdc129aa
Asen Penchev <asen.penchev2@gmail.com>
2019-05-16 15:29:50 +0300

    OOP - Homework 2 - Task 1

---
+++ OOP/Defining_Classes_P2/Task1/Defining_Clases_Part2.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    using System;
+
+    class DefiningClasesPart2
+    {
+        static void Main()
+        {
+            Point3D firstPoint = new Point3D(1, 2, 3);
+            Console.WriteLine(firstPoint.ToString());
+        }
+    }
+}
---
+++ OOP/Defining_Classes_P2/Task1/Point3D.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    using System;
+    struct Point3D
+    {
+        public Point3D(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - x coordinate\n{1} - y coordinate\n{2} - z coordinate\n ", this.X, this.Y, this.Z); 
+        }
+
+    }
+}
