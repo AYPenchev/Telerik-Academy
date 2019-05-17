0e0764e5aef2ba34bbdae875136d20c85194f7c7
Asen Penchev <asen.penchev2@gmail.com>
2019-05-17 14:40:37 +0300

    OOP - Homewor 1 - Task6

--- OOP/Defining_Classes_P2/Task1/Defining_Clases_Part2.cs
+++ OOP/Defining_Classes_P2/Task1/Defining_Clases_Part2.cs
@@ -6,32 +6,52 @@
     {
         static void Main()
         {
-            Point3D firstPoint = new Point3D(1, 2, 3);
-            Console.WriteLine(firstPoint.ToString());
+            //Point3D firstPoint = new Point3D(1, 2, 3);
+            //Console.WriteLine(firstPoint.ToString());
 
-            Point3D secondPoint = new Point3D(4, 5, 6);
-            Console.WriteLine(secondPoint.ToString());
+            //Point3D secondPoint = new Point3D(4, 5, 6);
+            //Console.WriteLine(secondPoint.ToString());
 
-            double distance = DistanceToPoint.CalculateDistance(firstPoint, secondPoint);
-            Console.WriteLine(distance + Environment.NewLine);
+            //double distance = DistanceToPoint.CalculateDistance(firstPoint, secondPoint);
+            //Console.WriteLine(distance + Environment.NewLine);
 
-            Path path = new Path(firstPoint, secondPoint, new Point3D(4, 4, 4));
-            Console.WriteLine(path);
+            //Path path = new Path(firstPoint, secondPoint, new Point3D(4, 4, 4));
+            //Console.WriteLine(path);
 
-            path.Add(new Point3D(5, 5, 5), new Point3D(3, 3, 3));
-            Console.WriteLine(path);
+            //path.Add(new Point3D(5, 5, 5), new Point3D(3, 3, 3));
+            //Console.WriteLine(path);
 
-            path.Remove(new Point3D(5, 5, 5));
-            Console.WriteLine(path);
+            //path.Remove(new Point3D(5, 5, 5));
+            //Console.WriteLine(path);
 
-            PathStorage.SaveToFile(path);
+            //PathStorage.SaveToFile(path);
 
-            path.Clear();
-            Console.WriteLine(path);
+            //path.Clear();
+            //Console.WriteLine(path);
 
-            Path newPath = new Path();
-            newPath = PathStorage.LoadFromFile();
-            Console.WriteLine(newPath);
+            //Path newPath = new Path();
+            //newPath = PathStorage.LoadFromFile();
+            //Console.WriteLine(newPath);
+
+            GenericList<int> list = new GenericList<int>();
+            list.AddAtEnd(9);
+            list.AddAtEnd(8);
+            list.AddAtEnd(9);
+
+            list.RemoveFirst(9);
+
+            Console.WriteLine(list.ToString());
+
+            list[1] = 33;
+            Console.WriteLine(list[1]);
+
+            list.AddAt(1, 22);
+            Console.WriteLine(list.ToString());
+
+            Console.WriteLine(list.GetElementIndex(22));
+
+            list.Clear();
+            Console.WriteLine(list.ToString());
         }
     }
 }
---
+++ OOP/Defining_Classes_P2/Task1/GenericList.cs
@@ -0,0 +1,127 @@
+﻿namespace Task1
+{
+    using System;
+    using System.Text;
+
+    class GenericList<T>
+        where T : IComparable
+    {
+        private T[] elements;
+        private int capacity;
+
+        public GenericList()
+        {
+            this.Count = 0;
+            this.Capacity = 8;
+            this.elements = new T[this.Capacity];
+        }
+
+        public T[] Elements { get; private set; }
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public void AddAtEnd(T element)
+        {
+            if(this.Capacity == this.Count)
+            {
+                this.Expand();
+            }
+            elements[this.Count] = element;
+            Count++;
+        }
+
+        public void AddAt(int index, T element)
+        {
+            if (this.Capacity == this.Count)
+            {
+                this.Expand();
+            }
+            for (int i = this.Count; i >= index; i--)
+            {
+                elements[i + 1] = elements[i];
+            }
+            elements[index] = element;
+            Count++;
+        }
+
+        public void RemoveFirst(T item)
+        {
+            int index = -1;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if(this.elements[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.elements[i] = this.elements[i + 1];
+            }
+
+            this.elements[this.Count - 1] = default(T);
+
+            this.Count--;
+        }
+
+        public void Clear()
+        {
+            while (this.Count - 1 >= 0)
+            {
+                RemoveAt(this.Count - 1);
+            }
+        }
+
+        public T this[int index]
+        {
+            get { return this.elements[index]; }
+            set { this.elements[index] = value; }
+        }
+
+        private void Expand()
+        {
+            this.Capacity *= 2;
+            T[] expandedArray = new T[this.Capacity];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                expandedArray[i] = elements[i];
+            }
+            this.elements = expandedArray;
+        }
+
+        public int GetElementIndex(T element)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                result.Append(this.elements[i]);
+
+                if (i != this.Count - 1)
+                {
+                    result.Append(" -> ");
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
