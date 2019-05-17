a13e4776f77d4ff2ea8767c97e04365b8bbe4670
Asen Penchev <asen.penchev2@gmail.com>
2019-05-17 10:55:54 +0300

    OOP - Homewor 1 - Task3

---
+++ OOP/Defining_Classes_P2/Task1/DistanceToPoint.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1
+{
+    using System;
+
+    static class DistanceToPoint
+    {
+        public static double CalculateDistance(Point3D firstPoint, Point3D secondPoint)
+        {
+            return Math.Sqrt(Math.Pow(secondPoint.X - firstPoint.X, 2) + Math.Pow(secondPoint.Y - firstPoint.Y, 2) + Math.Pow(secondPoint.Z - firstPoint.Z, 2));
+        }   
+    }
+}
--- OOP/Defining_Classes_P2/Task1/Point3D.cs
+++ OOP/Defining_Classes_P2/Task1/Point3D.cs
@@ -3,21 +3,30 @@
     using System;
     struct Point3D
     {
-        private readonly Point3D O = new Point3D(0, 0, 0);
+        private static readonly Point3D o = new Point3D(0, 0, 0);
+        
+        static Point3D()
+        {
+            o.X = 0;
+            o.Y = 0;
+            o.Z = 0;
+        }
+
         public Point3D(int x, int y, int z)
         {
             this.X = x;
             this.Y = y;
             this.Z = z;
         }
+
         public int X { get; private set; }
         public int Y { get; private set; }
         public int Z { get; private set; }
+        public static Point3D O { get; private set; }
 
         public override string ToString()
         {
             return string.Format("{0} - x coordinate\n{1} - y coordinate\n{2} - z coordinate\n ", this.X, this.Y, this.Z); 
         }
-
     }
 }
