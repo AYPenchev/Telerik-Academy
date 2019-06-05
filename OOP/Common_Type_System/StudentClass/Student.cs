80b2cecac3852cf1e37c2c8e214200d766cae692
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-05 03:44:17 -0400

    OOP - Homework 5 - Task 1

--- OOP/Common_Type_System/StudentClass/Student.cs
+++ OOP/Common_Type_System/StudentClass/Student.cs
@@ -41,19 +41,32 @@
         public string Email { get; private set; }
         public string Course { get; private set; }
 
+        public static bool operator ==(Student firstStudent, Student secondStudent)
+        {
+            return Student.Equals(firstStudent, secondStudent);
+        }
+
+        public static bool operator !=(Student firstStudent, Student secondStudent)
+        {
+            return !Student.Equals(firstStudent, secondStudent);
+        }
+
         public override bool Equals(object obj)
         {
-            return base.Equals(obj);
+            var otherStudent = (Student) obj;
+            return (this.SSN == otherStudent.SSN) && (this.PermanentAddress == otherStudent.PermanentAddress);
         }
 
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+            return SSN.GetHashCode() ^ PermanentAddress.GetHashCode();
         }
 
         public override string ToString()
         {
-            return base.ToString();
+            return string.Format("First name:  {0} \nMiddle name:  {1}  \nLast name:  {2} \nSocial Security number:  {3} " +
+                                 "\nPermanent Address: {4} \nPhone Number:  {5} \nEmail:  {6} \nCourse:  {7}"
+                , this.FirstName, this.MiddleName, this.LastName, this.SSN, this.PermanentAddress, this.PhoneNumber, this.Email, this.Course);
         }
     }
 }
