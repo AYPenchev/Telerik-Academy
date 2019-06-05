8500bb901096da0ce45f4829456d05ab37ef8495
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-05 05:03:02 -0400

    OOP - Homework 5 - Task 3

---
+++ OOP/Common_Type_System/Task2/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace Task2
+{
+    using System;
+
+    public class Person
+    {
+        public Person()
+        {
+            this.Name = null;
+            this.Age = null;
+        }
+
+        public Person(string name = null, int? age = null)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int? Age { get; private set; }
+
+        public override string ToString()
+        {
+            try
+            {
+                if (this.Age == null)
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                return string.Format("First name:  {0} \nAge is not defined! {1}", this.Name, e.Message);
+            }
+
+            return string.Format("First name:  {0} \nAge:  {1} ", this.Name, this.Age);
+        }
+    }
+}
---
+++ OOP/Common_Type_System/Task2/TestPersonClass.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2
+{
+    using System;
+
+    class TestPersonClass
+    {
+        static void Main()
+        {
+            Person firstPerson = new Person("Ivan", 5);
+            Person secondPerson = new Person("Peter");
+
+            Console.WriteLine(firstPerson.ToString());
+            Console.WriteLine(secondPerson.ToString());
+        }
+    }
+}
