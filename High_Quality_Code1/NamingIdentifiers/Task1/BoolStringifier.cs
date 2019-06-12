4103a20791f7755922c34cceb275e7dde76ad64b
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-12 04:31:56 -0400

    HQC - Homework Naming Identifiers - Task 1

---
+++ High_Quality_Code1/NamingIdentifiers/Task1/BoolStringifier.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    using System;
+
+    public class BoolStringifier
+    {
+        public void GetBoolAsString(bool boolToBeStringify)
+        {
+            string stringifiedBool = boolToBeStringify.ToString();
+            Console.WriteLine(stringifiedBool);
+        }
+    }
+}
---
+++ High_Quality_Code1/NamingIdentifiers/Task1/TestBoolStringifier.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    public class TestBoolStringifier
+    {
+        private const int MAX_COUNT = 6;
+
+        public static void Main()
+        {
+            BoolStringifier boolToString = new BoolStringifier();
+            boolToString.GetBoolAsString(true);
+        }
+    }
+}
