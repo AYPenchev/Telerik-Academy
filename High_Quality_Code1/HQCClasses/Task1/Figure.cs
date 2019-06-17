8263ebcfbf2b7a23c1b2bba8147c35c5b60fc016
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-17 11:56:10 -0400

    HQC - Homework HQC Classes - Task 1

---
+++ High_Quality_Code1/HQCClasses/Task1/Figure.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    public abstract class Figure
+    {
+        public Figure()
+        {
+        }
+
+        public Figure(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public virtual double Width { get; set; }
+        public virtual double Height { get; set; }
+        public virtual double Radius { get; set; }
+    }
+}
