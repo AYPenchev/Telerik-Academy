ff38e687cb3e90d672779b01586082991f671424
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-18 03:59:33 -0400

    HQC - Homework HQC Classes - Task 1

--- High_Quality_Code1/HQCClasses/Task1/Circle.cs
+++ High_Quality_Code1/HQCClasses/Task1/Circle.cs
@@ -4,48 +4,48 @@
 
     public class Circle : Figure
     {
-        public double Radius { get; set; }
-
+        private double radius;
         public Circle()
         {
         }
 
         public Circle(double radius)
         {
-            this.Radius = radius;
+            try
+            {
+                this.Radius = radius;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        public override double Width
+        public double Radius
         {
             get
             {
-                throw new NotImplementedException("Circle does not have Width");
+                return this.radius;
+
             }
             set
             {
-                throw new NotImplementedException("Circle does not have Width");
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.radius = value;
             }
         }
 
-        public override double Height
-        {
-            get
-            {
-                throw new NotImplementedException("Circle does not have Height");
-            }
-            set
-            {
-                throw new NotImplementedException("Circle does not have Height");
-            }
-        }
-
-        public double CalcPerimeter()
+        public override double CalcPerimeter()
         {
             double perimeter = 2 * Math.PI * this.Radius;
             return perimeter;
         }
 
-        public double CalcSurface()
+        public override double CalcSurface()
         {
             double surface = Math.PI * this.Radius * this.Radius;
             return surface;
--- High_Quality_Code1/HQCClasses/Task1/Rectangle.cs
+++ High_Quality_Code1/HQCClasses/Task1/Rectangle.cs
@@ -4,35 +4,68 @@
 
     class Rectangle : Figure
     {
+        private double width;
+        private double height;
+
         public Rectangle()
-            : base(0, 0)
         {
         }
 
+        //Is this okay?
         public Rectangle(double width, double height)
-            : base(width, height)
         {
+            try
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        public override double Radius
+        public double Width
         {
             get
             {
-                throw new NotImplementedException("Rectangle does not have Radius");
+                return this.width;
             }
             set
             {
-                throw new NotImplementedException("Rectangle does not have Radius");
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.width = value;
             }
         }
 
-        public double CalcPerimeter()
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.height = value;
+            }
+        }
+
+        public override double CalcPerimeter()
         {
             double perimeter = 2 * (this.Width + this.Height);
             return perimeter;
         }
 
-        public double CalcSurface()
+        public override double CalcSurface()
         {
             double surface = this.Width * this.Height;
             return surface;
