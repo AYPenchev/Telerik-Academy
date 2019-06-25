a1cb1f449e5a8d82d40e4d1f344393dfccab5813
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-25 04:01:46 -0400

    HQC2 - Homework Code Tuning - Task 4

--- High_Quality_Code2/CodeTuning/Task4/SortUtils.cs
+++ High_Quality_Code2/CodeTuning/Task4/SortUtils.cs
@@ -5,34 +5,23 @@
     public static class SortUtils<T>
         where T : IComparable
     {
-        /* This function takes last element as pivot, 
-         places the pivot element at its correct 
-         position in sorted array, and places all 
-         smaller (smaller than pivot) to left of 
-         pivot and all greater elements to right 
-         of pivot */
         private static int Partition(T[] arr, int low, int high)
         {
             T pivot = arr[high];
 
-            // index of smaller element 
             int i = (low - 1);
             for (int j = low; j < high; j++)
             {
-                // If current element is smaller  
-                // than or equal to pivot 
                 if (arr[j].CompareTo(pivot) <= 0)
                 {
                     i++;
 
-                    // swap arr[i] and arr[j] 
                     T swapPlaceHolder = arr[i];
                     arr[i] = arr[j];
                     arr[j] = swapPlaceHolder;
                 }
             }
 
-            // swap arr[i+1] and arr[high] (or pivot) 
             T swapSecondPlaceHoleder = arr[i + 1];
             arr[i + 1] = arr[high];
             arr[high] = swapSecondPlaceHoleder;
@@ -40,22 +29,12 @@
             return i + 1;
         }
 
-
-        /* The main function that implements QuickSort() 
-        arr[] --> Array to be sorted, 
-        low --> Starting index, 
-        high --> Ending index */
         public static void QuickSort(T[] arr, int low, int high)
         {
             if (low < high)
             {
-
-                /* pi is partitioning index, arr[pi] is  
-                now at right place */
                 int pi = Partition(arr, low, high);
 
-                // Recursively sort elements before 
-                // partition and after partition 
                 QuickSort(arr, low, pi - 1);
                 QuickSort(arr, pi + 1, high);
             }
@@ -63,14 +42,11 @@
 
         public static void SelectionSort(T[] arr)
         {
-            int n = arr.Length;
-
-            // One by one move boundary of unsorted subarray 
-            for (int i = 0; i < n - 1; i++)
+            int arrayLength = arr.Length;
+            for (int i = 0; i < arrayLength - 1; i++)
             {
-                // Find the minimum element in unsorted array 
                 int min_idx = i;
-                for (int j = i + 1; j < n; j++)
+                for (int j = i + 1; j < arrayLength; j++)
                 {
                     if (arr[j].CompareTo(arr[min_idx]) < 0)
                     {
@@ -78,8 +54,6 @@
                     }
                 }
 
-                // Swap the found minimum element with the first 
-                // element 
                 T swapPlaceHolder = arr[min_idx];
                 arr[min_idx] = arr[i];
                 arr[i] = swapPlaceHolder;
@@ -88,16 +62,12 @@
 
         public static void InsertionSort(T[] arr)
         {
-            int n = arr.Length;
-            for (int i = 1; i < n; ++i)
+            int arrayLength = arr.Length;
+            for (int i = 1; i < arrayLength; ++i)
             {
                 T key = arr[i];
                 int j = i - 1;
 
-                // Move elements of arr[0..i-1], 
-                // that are greater than key, 
-                // to one position ahead of 
-                // their current position 
                 while (j >= 0 && arr[j].CompareTo(key) > 0)
                 {
                     arr[j + 1] = arr[j];
---
+++ High_Quality_Code2/CodeTuning/Task4/SortingPerformance.cs
@@ -0,0 +1,132 @@
+﻿namespace Task4
+{
+    using System;
+    using System.Diagnostics;
+
+    public class SortingPerformance
+    {
+        public enum SortingDegree
+        {
+            Random,
+            Sorted,
+            Reversed
+        }
+
+        public static void GenericArrayInsertionSort<T>(T[] arrayToBeSorted, SortingDegree degree)
+            where T : IComparable
+        {
+            var arrayInsertionSortWatch = new Stopwatch();
+            arrayInsertionSortWatch.Start();
+
+            SortUtils<T>.InsertionSort(arrayToBeSorted);
+            Console.Write($"{degree} {typeof(T)} array sorted with insertion sort: ");
+            PrintUtils<T>.PrintArray(arrayToBeSorted);
+
+            arrayInsertionSortWatch.Stop();
+            Console.WriteLine($"{arrayInsertionSortWatch.Elapsed} \n");
+        }
+
+        public static void GenericArraySelectionSort<T>(T[] arrayToBeSorted, SortingDegree degree)
+            where T : IComparable
+        {
+            var arraySelectionSortWatch = new Stopwatch();
+            arraySelectionSortWatch.Start();
+
+            SortUtils<T>.SelectionSort(arrayToBeSorted);
+            Console.Write($"{degree} {typeof(T)} array sorted with selection sort: ");
+            PrintUtils<T>.PrintArray(arrayToBeSorted);
+
+            arraySelectionSortWatch.Stop();
+            Console.WriteLine($"{arraySelectionSortWatch.Elapsed} \n");
+        }
+
+        public static void GenericArrayQuickSort<T>(T[] arrayToBeSorted, SortingDegree degree) 
+            where T : IComparable
+        {
+            var arrayQuickSortWatch = new Stopwatch();
+            arrayQuickSortWatch.Start();
+
+            SortUtils<T>.QuickSort(arrayToBeSorted, 0, arrayToBeSorted.Length - 1);
+            Console.Write($"{degree} {typeof(T)} array sorted with quick sort: ");
+            PrintUtils<T>.PrintArray(arrayToBeSorted);
+
+            arrayQuickSortWatch.Stop();
+            Console.WriteLine($"{arrayQuickSortWatch.Elapsed} \n");
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Different sorts with different primitive types and degree of sorting - random.");
+
+            int[] randomIntArrayToBeInsertionSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] randomDoubleArrayToBeInsertionSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] randomStringArrayToBeInsertionSorted = { "b", "a", "d", "f", "c", "e" };
+            int[] randomIntArrayToBeSelectionSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] randomDoubleArrayToBeSelectionSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] randomStringArrayToBeSelectionSorted = { "b", "a", "d", "f", "c", "e" };
+            int[] randomIntArrayToBeQuickSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] randomDoubleArrayToBeQuickSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] randomStringArrayToBeQuickSorted = { "b", "a", "d", "f", "c", "e" };
+
+            GenericArrayInsertionSort(randomIntArrayToBeInsertionSorted, SortingDegree.Random);
+            GenericArrayInsertionSort(randomDoubleArrayToBeInsertionSorted, SortingDegree.Random);
+            GenericArrayInsertionSort(randomStringArrayToBeInsertionSorted, SortingDegree.Random);
+            
+            GenericArraySelectionSort(randomIntArrayToBeSelectionSorted, SortingDegree.Random);
+            GenericArraySelectionSort(randomDoubleArrayToBeSelectionSorted, SortingDegree.Random);
+            GenericArraySelectionSort(randomStringArrayToBeSelectionSorted, SortingDegree.Random);
+
+            GenericArrayQuickSort(randomIntArrayToBeQuickSorted, SortingDegree.Random);
+            GenericArrayQuickSort(randomDoubleArrayToBeQuickSorted, SortingDegree.Random);
+            GenericArrayQuickSort(randomStringArrayToBeQuickSorted, SortingDegree.Random);
+
+            Console.WriteLine("Different sorts with different primitive types and degree of sorting - sorted.");
+
+            int[] sortedIntArrayToBeInsertionSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] sortedDoubleArrayToBeInsertionSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] sortedStringArrayToBeInsertionSorted = { "b", "a", "d", "f", "c", "e" };
+            int[] sortedIntArrayToBeSelectionSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] sortedDoubleArrayToBeSelectionSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] sortedStringArrayToBeSelectionSorted = { "b", "a", "d", "f", "c", "e" };
+            int[] sortedIntArrayToBeQuickSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] sortedDoubleArrayToBeQuickSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] sortedStringArrayToBeQuickSorted = { "b", "a", "d", "f", "c", "e" };
+
+            GenericArrayInsertionSort(sortedIntArrayToBeInsertionSorted, SortingDegree.Sorted);
+            GenericArrayInsertionSort(sortedDoubleArrayToBeInsertionSorted, SortingDegree.Sorted);
+            GenericArrayInsertionSort(sortedStringArrayToBeInsertionSorted, SortingDegree.Sorted);
+
+            GenericArraySelectionSort(sortedIntArrayToBeSelectionSorted, SortingDegree.Sorted);
+            GenericArraySelectionSort(sortedDoubleArrayToBeSelectionSorted, SortingDegree.Sorted);
+            GenericArraySelectionSort(sortedStringArrayToBeSelectionSorted, SortingDegree.Sorted);
+
+            GenericArrayQuickSort(sortedIntArrayToBeQuickSorted, SortingDegree.Sorted);
+            GenericArrayQuickSort(sortedDoubleArrayToBeQuickSorted, SortingDegree.Sorted);
+            GenericArrayQuickSort(sortedStringArrayToBeQuickSorted, SortingDegree.Sorted);
+
+            Console.WriteLine("Different sorts with different primitive types and degree of sorting - reversed.");
+
+            int[] reversedIntArrayToBeInsertionSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] reversedDoubleArrayToBeInsertionSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] reversedStringArrayToBeInsertionSorted = { "b", "a", "d", "f", "c", "e" };
+            int[] reversedIntArrayToBeSelectionSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] reversedDoubleArrayToBeSelectionSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] reversedStringArrayToBeSelectionSorted = { "b", "a", "d", "f", "c", "e" };
+            int[] reversedIntArrayToBeQuickSorted = { 10, 7, 8, 9, 1, 5 };
+            double[] reversedDoubleArrayToBeQuickSorted = { 10.1, 7.2, 8.3, 9.4, 1.5, 5.6 };
+            string[] reversedStringArrayToBeQuickSorted = { "b", "a", "d", "f", "c", "e" };
+
+            GenericArrayInsertionSort(reversedIntArrayToBeInsertionSorted, SortingDegree.Reversed);
+            GenericArrayInsertionSort(reversedDoubleArrayToBeInsertionSorted, SortingDegree.Reversed);
+            GenericArrayInsertionSort(reversedStringArrayToBeInsertionSorted, SortingDegree.Reversed);
+
+            GenericArraySelectionSort(reversedIntArrayToBeSelectionSorted, SortingDegree.Reversed);
+            GenericArraySelectionSort(reversedDoubleArrayToBeSelectionSorted, SortingDegree.Reversed);
+            GenericArraySelectionSort(reversedStringArrayToBeSelectionSorted, SortingDegree.Reversed);
+
+            GenericArrayQuickSort(reversedIntArrayToBeQuickSorted, SortingDegree.Reversed);
+            GenericArrayQuickSort(reversedDoubleArrayToBeQuickSorted, SortingDegree.Reversed);
+            GenericArrayQuickSort(reversedStringArrayToBeQuickSorted, SortingDegree.Reversed);
+        }
+    }
+}
